0960391446f63702d4c6166b3b2c3874d1f8fe47
ladyp94 <ladypulido94@gmail.com>
2025-11-06 04:55:08 -0700

    Impl(ph3): DataAccess file ready

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,20 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+public interface DataAccess {
+    void clear();
+    void addUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    int addGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameId) throws DataAccessException;
+    Collection<GameData> getAllGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+    void addAuth(AuthData authToken) throws DataAccessException;
+    AuthData getAuthToken(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+}
