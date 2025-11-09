a2459b89c0c0c6959e8112dd5dc8e1eb8a8b7030
ladyp94 <ladypulido94@gmail.com>
2025-11-09 03:02:04 -0700

    Impl(ph3): The ClearService and UserService are done. Working on the tests for each one

---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,16 @@
+package service;
+
+import dataaccess.DataAccess;
+
+
+public class ClearService {
+    private final DataAccess dataAccess;
+
+    public ClearService(DataAccess dataAccess){
+        this.dataAccess = dataAccess;
+    }
+
+    public void clear(){
+        dataAccess.clear();
+    }
+}
---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,67 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+
+import java.util.UUID;
+
+public class UserService {
+
+    private final DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess){
+        this.dataAccess = dataAccess;
+    }
+
+    public AuthData register(UserData user) throws DataAccessException{
+
+        if (user.username() == null || user.username().isEmpty() ||
+        user.password() == null || user.password().isEmpty() ||
+        user.email() == null || user.email().isEmpty()){
+            throw new DataAccessException("Error: Bad Request");
+        }
+
+        UserData existingUser = dataAccess.getUser(user.username());
+
+        if(existingUser != null){
+            throw new DataAccessException("Error: User already taken");
+        }
+
+        dataAccess.addUser(user);
+
+        String authenticator = UUID.randomUUID().toString();
+        AuthData authToken = new AuthData(authenticator, user.username());
+        dataAccess.addAuthToken(authToken);
+
+        return authToken;
+    }
+
+    public AuthData login (UserData user) throws DataAccessException{
+        if (user.username() == null || user.username().isEmpty() ||
+        user.password() == null || user.password().isEmpty()){
+            throw new DataAccessException("Error: Bad Request");
+        }
+
+        UserData existingUser = dataAccess.getUser(user.username());
+
+        if(!user.password().equals(existingUser.password())){
+            throw new DataAccessException("Error: User not found");
+        }
+
+        String authenticator = UUID.randomUUID().toString();
+        AuthData authToken = new AuthData(authenticator, user.username());
+        dataAccess.addAuthToken(authToken);
+
+        return authToken;
+    }
+
+    public void logout (String authToken) throws DataAccessException {
+        if (authToken == null || authToken.isEmpty()){
+            throw new DataAccessException("Error: AuthToken doesn't exist");
+        }
+
+        dataAccess.deleteAuthToken(authToken);
+    }
+}
