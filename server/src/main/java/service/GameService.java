1a0d957c48f3beef9a16e6e90a74a7c68601a350
ladyp94 <ladypulido94@gmail.com>
2025-11-17 12:30:05 -0700

    Impl(ph3): Currently working on the joinGame method and the tests

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,34 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
+
 public class GameService {
+
+    private final DataAccess dataAccess;
+
+    public GameService (DataAccess dataAccess){
+        this.dataAccess = dataAccess;
+    }
+
+    public int createGame(AuthData token, String gameName) throws DataAccessException{
+
+        if(token.authToken() == null || token.authToken().isEmpty()
+                || dataAccess.getAuthToken(token.authToken()) == null){
+            throw new DataAccessException("Error: Unauthorize");
+        }
+
+        if(gameName.isEmpty()){
+            throw new DataAccessException("Error: Bad Request");
+        }
+
+        ChessGame chessGame = new ChessGame();
+        GameData game = new GameData(0, null, null, gameName, chessGame);
+
+        return dataAccess.addGame(game);
+    }
+    
 }
--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,4 +1,48 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameServiceTests {
+    private MemoryDataAccess dao;
+    private GameService gameService;
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp(){
+        dao = new MemoryDataAccess();
+        gameService = new GameService(dao);
+        userService = new UserService(dao);
+    }
+
+    @Test
+    public void positiveCreateGame() throws DataAccessException {
+        UserData user = new UserData("test", "testPassword","test@test.com");
+        AuthData authToken = userService.register(user);
+
+        int gameId = gameService.createGame(authToken, "testGame");
+        assertTrue(gameId > 0);
+        GameData game = dao.getGame(gameId);
+        assertNotNull(game);
+        assertEquals("testGame", game.gameName());
+
+    }
+
+    @Test
+    public void negativeCreateGame() throws DataAccessException{
+        AuthData wrongToken = new AuthData("invalidToken", "user");
+        assertThrows(DataAccessException.class,
+                () -> gameService.createGame(wrongToken, "testName"));
+        UserData user = new UserData("test", "testPassword", "test@test.com");
+        AuthData authToken = userService.register(user);
+        assertThrows(DataAccessException.class, () -> gameService.createGame(authToken, ""));
+
+    }
 }
