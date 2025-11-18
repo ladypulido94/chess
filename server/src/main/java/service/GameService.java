37578924f727240b427638c1f9ac6d06b9da29fa
ladyp94 <ladypulido94@gmail.com>
2025-11-18 00:22:21 -0700

    Impl(ph3): The GameService file is done and all the tests pass.

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -6,6 +6,8 @@ import dataaccess.DataAccessException;
 import model.AuthData;
 import model.GameData;
 
+import java.util.Collection;
+
 public class GameService {
 
     private final DataAccess dataAccess;
@@ -30,5 +32,58 @@ public class GameService {
 
         return dataAccess.addGame(game);
     }
-    
+
+    public void joinGame(String authToken, String playerColor,int gameID) throws DataAccessException {
+        GameData game = dataAccess.getGame(gameID);
+        AuthData token = dataAccess.getAuthToken(authToken);
+
+        if(token == null){
+            throw new DataAccessException("Error: Unauthorized");
+        }
+
+        if(game == null){
+            throw new DataAccessException("Error: Bad Request");
+        }
+
+        playerColor = playerColor.toUpperCase();
+
+        if(playerColor == null || (!playerColor.equals("WHITE") && !playerColor.equals("BLACK"))){
+            throw new DataAccessException("Error: Bad Request");
+        }
+
+        String color = playerColor.toUpperCase();
+
+        if(color.equals("WHITE") && game.whiteUsername() != null){
+            throw new DataAccessException("Error: Already Taken");
+        }
+
+        if(color.equals("BLACK") && game.blackUsername() != null){
+            throw new DataAccessException("Error: Already Taken");
+        }
+
+        String username = token.username();
+        GameData updatedGame;
+
+        if(color.equals("WHITE")){
+            updatedGame = new GameData(game.gameID(), username, game.blackUsername(), game.gameName(), game.game());
+        } else {
+            updatedGame = new GameData(game.gameID(), game.whiteUsername(), username, game.gameName(), game.game());
+        }
+
+        dataAccess.updateGame(updatedGame);
+
+    }
+
+    public Collection<GameData> listAllGames(String authToken) throws DataAccessException{
+        AuthData token = dataAccess.getAuthToken(authToken);
+
+        if(token == null){
+            throw new DataAccessException("Error: Unauthorized");
+        }
+
+        Collection<GameData> games = dataAccess.getAllGames();
+
+        return games;
+    }
+
 }
--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -8,6 +8,8 @@ import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameServiceTests {
@@ -43,6 +45,54 @@ public class GameServiceTests {
         UserData user = new UserData("test", "testPassword", "test@test.com");
         AuthData authToken = userService.register(user);
         assertThrows(DataAccessException.class, () -> gameService.createGame(authToken, ""));
+
+    }
+
+    @Test
+    public void positiveUpdateGame() throws DataAccessException{
+        UserData user = new UserData("test", "testPassword","test@test.com");
+        AuthData token = userService.register(user);
+
+        int gameId = gameService.createGame(token, "testGame");
+
+        assertDoesNotThrow(() -> gameService.joinGame(token.authToken(),"white", gameId));
+
+        GameData updatedGame = dao.getGame(gameId);
+
+        assertEquals("test", updatedGame.whiteUsername());
+        assertNull(updatedGame.blackUsername());
+    }
+
+    @Test
+    public void negativeUpdateGame() throws DataAccessException{
+        UserData user = new UserData("test", "testPassword", "test@test.com");
+        AuthData token = userService.register(user);
+
+        int gameId = gameService.createGame(token, "testGame");
+
+        assertThrows(DataAccessException.class, () -> gameService.joinGame(token.authToken(),"purple",gameId));
+    }
+
+    @Test
+    public void positiveListAllGames() throws DataAccessException {
+        UserData user = new UserData("test", "testPassword", "test@test.com");
+        AuthData token = userService.register(user);
+
+        gameService.createGame(token, "testGame1");
+        gameService.createGame(token, "testGame2");
+        gameService.createGame(token, "testGame3");
+
+        Collection<GameData> games = gameService.listAllGames(token.authToken());
+
+        assertNotNull(games);
+    }
+
+    @Test
+    public void negativeListAllGames() throws DataAccessException{
+
+        String invalidToken = "InvalidToken";
+
+        assertThrows(DataAccessException.class, () -> gameService.listAllGames(invalidToken));
 
     }
 }
