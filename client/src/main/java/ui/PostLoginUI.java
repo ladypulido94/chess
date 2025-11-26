a60f1c983b1ef74c8a10b5e5265d07b94f267695
ladyp94 <ladypulido94@gmail.com>
2025-11-26 00:13:32 -0700

    Impl(ph5): PreLogout is done. Next is the ChessBoard

--- client/src/main/java/ui/PostLoginUI.java
+++ client/src/main/java/ui/PostLoginUI.java
@@ -2,7 +2,11 @@ package ui;
 
 import facade.ServerFacade;
 import model.AuthData;
+import model.GameData;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Scanner;
 
 public class PostLoginUI {
@@ -10,6 +14,8 @@ public class PostLoginUI {
     private final AuthData authData;
     private final Scanner scanner;
     private boolean running;
+
+    private List<GameData> games = new ArrayList<>();
 
     public PostLoginUI(ServerFacade facade, AuthData authData){
         this.facade = facade;
@@ -40,29 +46,137 @@ public class PostLoginUI {
     private void handleHelp(){
         System.out.println("create <NAME> - a game");
         System.out.println("list - games");
-        System.out.println("join <ID> - a game");
+        System.out.println("join <ID> [WHITE|BLACK] - a game");
         System.out.println("observe <ID> - a game");
         System.out.println("logout - when you are done");
-        System.out.println("quit - playing");
+        System.out.println("quit - playing chess");
+        System.out.println("help - with possible commands");
     }
 
     private void handleCreate(String[] tokens){
+        if(tokens.length != 2){
+            System.out.println("Usage: create <NAME>");
+            return;
+        }
 
+        try{
+            facade.createGame(authData.authToken(), tokens[1]);
+
+        } catch (Exception e){
+            System.out.println("Create failed: " + e.getMessage());
+        }
     }
 
     private void handleList(){
+        try{
+            games = new ArrayList<>(facade.listAllGames(authData.authToken()));
 
+            if(games.isEmpty()){
+                System.out.println("No games available.");
+                return;
+            }
+
+            for(int i = 0; i < games.size(); i++){
+                GameData game = games.get(i);
+                int number = i + 1;
+                String whiteUsername;
+                String blackUsername;
+
+                if(game.whiteUsername() != null){
+                    whiteUsername = game.whiteUsername();
+                } else {
+                    whiteUsername = "(empty)";
+                }
+
+                if(game.blackUsername() != null){
+                    blackUsername = game.blackUsername();
+                } else {
+                    blackUsername = "(empty)";
+                }
+
+                System.out.println(number + ". " + game.gameName() +
+                        " - White: " + whiteUsername + ", Black: " + blackUsername);
+            }
+
+        } catch (Exception e){
+            System.out.println("List failed: " + e.getMessage());
+        }
     }
 
     private void handleJoin(String[] tokens){
+        if(tokens.length != 3){
+            System.out.println("Usage: join <ID> [WHITE|BLACK]");
+            return;
+        }
+
+        if(games.isEmpty()){
+            System.out.println("No games available. User 'list' first.");
+            return;
+        }
+
+        try{
+            int gameNumber = Integer.parseInt(tokens[1]);
+
+            if(gameNumber < 1 || gameNumber > games.size()){
+                System.out.println("Invalid game number.");
+                return;
+            }
+
+            String playerColor = tokens[2].toUpperCase();
+
+            if(!playerColor.equals("WHITE") && !playerColor.equals("BLACK")){
+                System.out.println("Invalid color. use WHITE or BLACK.");
+                return;
+            }
+
+            GameData game = games.get(gameNumber - 1);
+            facade.joinGame(authData.authToken(), game.gameID(), playerColor);
+            System.out.println("Joined game as " + playerColor);
+
+            //TO DO: DRAW CHESSBOARD
+
+        } catch (Exception e){
+            System.out.println("Join failed: " + e.getMessage());
+        }
 
     }
 
     private void handleObserve(String[] tokens){
+        if(tokens.length != 2){
+            System.out.println("Usage: observe <ID>");
+            return;
+        }
+
+        if(games.isEmpty()){
+            System.out.println("No games available. Use 'list' first");
+            return;
+        }
+
+        try{
+            int gameNumber = Integer.parseInt(tokens[1]);
+            if(gameNumber < 1 || gameNumber > games.size()){
+                System.out.println("Invalid game number.");
+                return;
+            }
+            GameData game = games.get(gameNumber - 1);
+            System.out.println("Observing game: " + game.gameName());
+
+            //TO DO: DRAW CHESSBOARD
+        } catch (Exception e) {
+            System.out.println("Observe failed: " + e.getMessage());
+        }
 
     }
 
     private void handleLogout(){
+        try{
+            facade.logout(authData.authToken());
+            System.out.println("Logged out");
+            running = false;
+
+        } catch (Exception e) {
+            System.out.println("Logout failed: " + e.getMessage());
+        }
 
     }
 
