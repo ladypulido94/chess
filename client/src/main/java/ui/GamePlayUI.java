e684a25ffbce638d91d72d1b2b20ac8f4e268edd
ladyp94 <ladypulido94@gmail.com>
2025-12-05 23:34:22 -0700

    Impl(ph6): Filled the notify Method with its method helpers

--- client/src/main/java/ui/GamePlayUI.java
+++ client/src/main/java/ui/GamePlayUI.java
@@ -3,9 +3,14 @@ package ui;
 import chess.ChessGame;
 import websocket.ServerMessageObserver;
 import websocket.WebSocketCommunicator;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
 import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
 
 public class GamePlayUI implements ServerMessageObserver {
     private final WebSocketCommunicator webSocketCommunicator;
@@ -29,10 +34,51 @@ public class GamePlayUI implements ServerMessageObserver {
 
     @Override
     public void notify(ServerMessage message) {
+        switch (message.getServerMessageType()){
+            case LOAD_GAME -> handleLoadGame((LoadGameMessage) message);
+            case ERROR -> handleError((ErrorMessage) message);
+            case NOTIFICATION -> handleNotification((NotificationMessage) message);
+        }
 
     }
 
     public void run(){
 
+    }
+
+    //Store the game and redraw the board
+    private void handleLoadGame(LoadGameMessage message){
+        this.currentGame = message.getGame();
+        drawBoard();
+    }
+
+    //Print Error in red
+    private void handleError(ErrorMessage message){
+        System.out.println(SET_TEXT_COLOR_RED + message.getErrorMessage() + RESET_TEXT_COLOR);
+
+    }
+
+    //Print notification in yellow
+    private void handleNotification(NotificationMessage message){
+        System.out.println(SET_TEXT_COLOR_YELLOW + message.getNotificationMessage() + RESET_TEXT_COLOR);
+
+    }
+
+    //Drawing the board from white or black perspective
+    private void drawBoard(){
+        if(currentGame == null){
+            System.out.println("No game loaded yet.");
+            return;
+        }
+
+        if("WHITE".equals(playerColor)){
+            ChessBoard.drawWhiteBoard(currentGame);
+
+        } else if("BLACK".equals(playerColor)){
+            ChessBoard.drawBlackBoard(currentGame);
+
+        } else {
+            ChessBoard.drawWhiteBoard(currentGame);
+        }
     }
 }
